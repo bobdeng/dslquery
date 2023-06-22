f753b4c5635676a68af6bf503c38129cf93056da
bobdeng <bobdeng@163.com>
2023-06-22 17:41:16 +0800

    test: add integration test

--- src/test/java/cn/beagile/dslquery/IntegrationTest.java
+++ src/test/java/cn/beagile/dslquery/IntegrationTest.java
@@ -4,8 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplication.class)
@@ -13,8 +22,60 @@ public class IntegrationTest {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
+    @View("person")
+    public static class Person {
+        @Column("name1")
+        private String name;
+        @Column("age")
+        private Integer age;
+    }
+
+    public static class SpringQueryExecutor implements QueryExecutor {
+        private final NamedParameterJdbcTemplate jdbcTemplate;
+
+        public SpringQueryExecutor(NamedParameterJdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+        @Override
+        public <T> List<T> execute(SQLQuery sqlQuery, Function<ResultSet, T> resultSetReader) {
+            String sql = sqlQuery.sql();
+            if (sqlQuery.limit() != null) {
+                sql += " limit " + sqlQuery.skip() + "," + sqlQuery.limit();
+            }
+            return jdbcTemplate.query(sql, sqlQuery.getParams(), (rs, rowNum) -> resultSetReader.apply(rs));
+        }
+    }
+
     @Test
     public void should_query() {
+        DSLQuery query = new DSLQuery(new SpringQueryExecutor(jdbcTemplate), Person.class);
+        List result = query.skip(0).limit(10).query();
+        assertEquals(2, result.size());
+    }
 
+    @Test
+    public void should_query_with_order() {
+        DSLQuery query = new DSLQuery(new SpringQueryExecutor(jdbcTemplate), Person.class);
+        List result = query.sort("age desc").skip(0).limit(10).query();
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void should_return1_when_add_where() {
+        DSLQuery query = new DSLQuery(new SpringQueryExecutor(jdbcTemplate), Person.class);
+        List<Person> result = query
+                .where("(and(name equal bob))")
+                .skip(0).limit(10).query();
+        assertEquals(1, result.size());
+        Person person = result.get(0);
+        assertEquals("bob", person.name);
+    }
+
+    @Test
+    public void should_return_empty_when_wrong_page() {
+        DSLQuery query = new DSLQuery(new SpringQueryExecutor(jdbcTemplate), Person.class);
+        List result = query.skip(10).limit(10).query();
+        assertEquals(0, result.size());
     }
 }
