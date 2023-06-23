1e3491513f710f589a12bf30e245ebb29f697c84
bobdeng <bobdeng@163.com>
2023-06-23 11:54:43 +0800

    test: cover fail set field

---
+++ src/test/java/cn/beagile/dslquery/ReflectFieldSetterTest.java
@@ -0,0 +1,17 @@
+package cn.beagile.dslquery;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReflectFieldSetterTest {
+    @Test
+    public void should_throw_when_final() {
+        RuntimeException e = assertThrows(RuntimeException.class, () -> new ReflectFieldSetter(new FinalFieldBean(), FinalFieldBean.class.getDeclaredFields()[0], "bob").set());
+        assertEquals("java.lang.IllegalAccessException: Can not set static final java.lang.String field cn.beagile.dslquery.ReflectFieldSetterTest$FinalFieldBean.name to null value", e.getMessage());
+    }
+
+    public static class FinalFieldBean {
+        private static final String name = "";
+    }
+}
