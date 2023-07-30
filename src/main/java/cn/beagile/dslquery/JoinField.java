419e4db752f977e2515300162a8bb3fb3882396e
Bob.Deng <dengzg1977@gmail.com>
2023-07-30 22:31:41 +0800

    refactor

--- src/main/java/cn/beagile/dslquery/JoinField.java
+++ src/main/java/cn/beagile/dslquery/JoinField.java
@@ -18,39 +18,98 @@ public class JoinField {
     }
 
     public String joinStatement() {
-        View view = field.getType().getAnnotation(View.class);
-        String alias = parents.stream()
-                .map(Field::getName).collect(Collectors.joining("_"));
-        JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
-        if (joinColumn != null) {
-            String joinTable = getJoinTable();
-            return "left join " + view.value() + " " + alias + " on " + alias + "." + joinColumn.referencedColumnName()
-                    + " = " + joinTable + "." + joinColumn.name();
-        } else {
-            JoinColumns joinColumns = field.getAnnotation(JoinColumns.class);
-            List<String> result = new ArrayList<>();
-            for (int i = 0; i < joinColumns.value().length; i++) {
-                joinColumn = joinColumns.value()[i];
-                if (i == 0) {
-                    String joinTable = getJoinTable();
-                    result.add("left join " + joinColumn.table() + " on " + joinColumn.table() + "." + joinColumn.referencedColumnName()
-                            + " = " + joinTable + "." + joinColumn.name());
-                } else {
-                    result.add("left join " + view.value() + " " + alias + " on " + alias + "." + joinColumn.referencedColumnName()
-                            + " = " + joinColumns.value()[i - 1].table() + "." + joinColumn.name());
-                }
-            }
-            return result.stream().collect(Collectors.joining("\n"));
+        if (field.isAnnotationPresent(JoinColumn.class)) {
+            return singleJoinStatement();
         }
+        return multiJoinStatement();
+    }
+
+    private String multiJoinStatement() {
+        List<String> result = new ArrayList<>();
+        JoinColumn[] columns = field.getAnnotation(JoinColumns.class).value();
+        for (int i = 0; i < columns.length; i++) {
+            JoinColumn preColumn = i == 0 ? null : columns[i - 1];
+            JoinColumn joinColumn = columns[i];
+            if (i == 0) {
+                result.add(JoinBuilder.joinBuilder().joinTable(joinColumn.table())
+                        .joinField(joinColumn.referencedColumnName())
+                        .onTable(getJoinTable())
+                        .onField(joinColumn.name()).build());
+            }
+            if (i == columns.length - 1) {
+                result.add(JoinBuilder.joinBuilder().joinTable(field.getType().getAnnotation(View.class).value())
+                        .joinTableAlias(getTableAlias())
+                        .joinField(joinColumn.referencedColumnName())
+                        .onTable(preColumn.table())
+                        .onField(joinColumn.name()).build());
+            }
+        }
+        return result.stream().collect(Collectors.joining("\n"));
+    }
+
+    private String getTableAlias() {
+        return parents.stream().map(Field::getName).collect(Collectors.joining("_"));
+    }
+
+    private String singleJoinStatement() {
+        JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
+        return JoinBuilder.joinBuilder().joinTable(field.getType().getAnnotation(View.class).value())
+                .joinTableAlias(getTableAlias())
+                .joinField(joinColumn.referencedColumnName())
+                .onTable(getJoinTable())
+                .onField(joinColumn.name()).build();
     }
 
     private String getJoinTable() {
-        String joinTable = "";
         if (parents.size() == 1) {
-            joinTable = field.getDeclaringClass().getAnnotation(View.class).value();
-        } else {
-            joinTable = parents.stream().map(Field::getName).limit(parents.size() - 1).collect(Collectors.joining("_"));
+            return field.getDeclaringClass().getAnnotation(View.class).value();
         }
-        return joinTable;
+        return parents.stream().map(Field::getName).limit(parents.size() - 1).collect(Collectors.joining("_"));
+    }
+
+    public static class JoinBuilder {
+        private String joinTable;
+        private String joinTableAlias;
+        private String onTable;
+        private String onField;
+        private String joinField;
+
+        public static JoinBuilder joinBuilder() {
+            return new JoinBuilder();
+        }
+
+        public JoinBuilder joinTable(String joinTable) {
+            this.joinTable = joinTable;
+            return this;
+        }
+
+        public JoinBuilder joinTableAlias(String joinTableAlias) {
+            this.joinTableAlias = joinTableAlias;
+            return this;
+        }
+
+        public JoinBuilder onTable(String onTable) {
+            this.onTable = onTable;
+            return this;
+        }
+
+        public JoinBuilder onField(String onField) {
+            this.onField = onField;
+            return this;
+        }
+
+        public JoinBuilder joinField(String joinField) {
+            this.joinField = joinField;
+            return this;
+        }
+
+        public String build() {
+            if (joinTableAlias == null) {
+                return "left join " + joinTable + " on " + joinTable + "." + joinField
+                        + " = " + onTable + "." + onField;
+            }
+            return "left join " + joinTable + " " + joinTableAlias + " on " + joinTableAlias + "." + joinField
+                    + " = " + onTable + "." + onField;
+        }
     }
 }
