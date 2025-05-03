62ebfc93559bdf3cf33bff602d5964d5d2f936bb
Bob.Deng <dengzg1977@gmail.com>
2025-05-03 14:32:21 +0800

    refactor: use java8

--- src/main/java/cn/beagile/dslquery/SingleExpression.java
+++ src/main/java/cn/beagile/dslquery/SingleExpression.java
@@ -3,7 +3,6 @@ package cn.beagile.dslquery;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 
 class SingleExpression implements FilterExpression {
@@ -82,18 +81,17 @@ class SingleExpression implements FilterExpression {
     @Override
     public String toSQL(List<SQLField> fields, RawSQLBuilder sqlWhere) {
         Operator operatorEnum = Operators.byName(this.operator);
-        Optional<String> sqlName = fields.stream()
+        return fields.stream()
                 .filter(field -> field.getName().equals(this.field))
                 .map(SQLField::getWhereName)
-                .findFirst();
-        if (sqlName.isEmpty()) {
-            return "true";
-        }
-        String[] paramNames = operatorEnum.params(sqlWhere);
-        if (operatorEnum.requireValue) {
-            addParams(sqlWhere, operatorEnum, paramNames);
-        }
-        return String.format(operatorEnum.whereFormat(), sqlName.get(), operatorEnum.operator, paramNames[0], paramNames[1]);
+                .findFirst()
+                .map(sqlName -> {
+                    String[] paramNames = operatorEnum.params(sqlWhere);
+                    if (operatorEnum.requireValue) {
+                        addParams(sqlWhere, operatorEnum, paramNames);
+                    }
+                    return String.format(operatorEnum.whereFormat(), sqlName, operatorEnum.operator, paramNames[0], paramNames[1]);
+                }).orElse("true");
 
     }
 }
