09cf87d6b6b5fa2d68b1949755a8a54e539c2bde
Bob.Deng <dengzg1977@gmail.com>
2025-09-11 11:25:56 +0800

    fix:

--- src/main/java/cn/beagile/dslquery/RawSQLBuilder.java
+++ src/main/java/cn/beagile/dslquery/RawSQLBuilder.java
@@ -22,8 +22,7 @@ public class RawSQLBuilder implements SQLBuilder {
         this.fields = fields;
         if (filters != null && filters.length > 0) {
             if (filters.length > 1) {
-                this.expression = new ComplexExpression("and",
-                        Arrays.stream(filters).map(filter -> new WhereParser().parse(filter)).collect(Collectors.toList())
+                this.expression = new ComplexExpression("and", Arrays.stream(filters).map(filter -> new WhereParser().parse(filter)).collect(Collectors.toList())
                 );
             } else {
                 this.expression = new WhereParser().parse(filters[0]);
