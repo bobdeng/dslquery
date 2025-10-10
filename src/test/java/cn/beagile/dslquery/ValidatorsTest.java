a10a7d1f436ceda0273676f78ab676968eb502c5
Bob.Deng <dengzg1977@gmail.com>
2025-10-10 20:13:13 +0800

    fix: validator

---
+++ src/test/java/cn/beagile/dslquery/ValidatorsTest.java
@@ -0,0 +1,22 @@
+package cn.beagile.dslquery;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorsTest {
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    "info.performances.totalPostCount30d",
+                    "abc",
+                    "ab.ce"
+            }
+    )
+    void 字段合法(String field) {
+        assertDoesNotThrow(() -> Validators.validateFieldName(field));
+
+    }
+}
